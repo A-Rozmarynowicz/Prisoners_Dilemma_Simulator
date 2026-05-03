6b5b75766e51ff03ad0490f0043f5441621559bf
A-Rozmarynowicz <arkadiuszrozmarynowicz@gmail.com>
2026-05-03 20:46:35 +0200

    Implementing reward system

--- source/utils/Environments.py
+++ source/utils/Environments.py
@@ -11,7 +11,16 @@ class Environment(Generic[Act]):
         return Actions
 
     def Duel(self) -> dict[int, Act]:
-        return ()
+        return {}
+
+    def Query_Players_Moves(self) -> dict[int, Act]:
+        return {}
+
+    def Reward(self, players_actions : dict[int, Act]) -> dict[int, int]:
+        rewards = {}
+        for i in players_actions.keys():
+            rewards[i] = 0
+        return rewards
 
 class Prison(Environment[Prison_Actions]):
     def __init__(self):
@@ -20,10 +29,17 @@ class Prison(Environment[Prison_Actions]):
     def Get_Actions(self) -> Type[Prison_Actions]:
         return Prison_Actions
 
-    def Duel(self, *players : Player[Prison_Actions]) ->  dict[int, Prison_Actions]:
-        players_actions = {}
+    def Duel(self, *players : Player[Prison_Actions]) ->  dict[int, int]:
+        players_actions = self.Query_Players_Moves(*players)
+        rewards = self.Reward(players_actions)
+        return rewards
+
+    def Query_Players_Moves(self, *players : Player[Prison_Actions]) ->  dict[int, Prison_Actions]:
+        players_actions : dict[int, Act]= {}
         for player in players:
             players_actions[player.Get_ID()] = player.Make_Move(self.action_history)
         self.action_history.Append_Players_Actions(players_actions)
         return players_actions
 
+    def Reward(self, players_actions):
+        return super().Reward(players_actions)
