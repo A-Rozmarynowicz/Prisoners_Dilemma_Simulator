b1c65cb6f1be9b7e4fb9e7abd3254a0764a8545b
A-Rozmarynowicz <arkadiuszrozmarynowicz@gmail.com>
2026-05-03 12:51:59 +0200

    Strategy class now doesnt store action space

--- source/utils/Player.py
+++ source/utils/Player.py
@@ -1,17 +1,19 @@
 from Strategy import Strategy
-from Action import Action
+from utils.Action import Action
+from typing import Type, Generic
 
-class Player():
-    def __init__(self):
-        pass
+class Player(Generic[Action]):
+    def __init__(self, ID : int, action_space : Type[Action]):
+        self.ID = ID
+        self.action_space = action_space
 
-    def Make_Move(self) -> Action:
+    def Make_Move(self, action_history : list[tuple[Action, ...]]) -> Action:
         return Action.Cooperate
 
 class Bot_Player(Player):
-    def __init__(self, strategy : Strategy):
-        super().__init__()
+    def __init__(self, ID : int, strategy : Strategy):
+        super().__init__(ID)
         self.strategy = strategy
 
-    def Make_Move(self, action_history : list) -> Action:
-        return self.strategy.Make_Move(action_history)
+    def Make_Move(self, ID : int, action_history : list[tuple[Action, ...]]) -> Action:
+        return self.strategy.Make_Move(ID=ID, action_space=self.action_space, action_history=action_history)
--- source/utils/Strategy.py
+++ source/utils/Strategy.py
@@ -4,19 +4,19 @@ import random
 
 
 class Strategy(Generic[Action]):
-    def __init__(self, action_space: Type[Action]):
-        self.action_space = action_space
+    def __init__(self):
+        pass
 
-    def Get_Random_Action(self) -> Action:
-        return random.choice(list(self.action_space))
+    def Get_Random_Action(self, action_space : Type[Action]) -> Action:
+        return random.choice(list(action_space))
 
-    def Get_Action_Space(self) -> Type[Action]:
-        return self.action_space
-
-    # def Make_Move(self, action_history : list[tuple[Action, ...]]) -> Action:
-    #     return list(self.action_space)[0]
+    def Make_Move(self, ID : int, action_space : Type[Action], action_history : list[tuple[Action, ...]]) -> Action:
+        return list(action_space)[0]
 
 class Random_Strategy(Strategy):
     def __init__(self):
         super().__init__()
+
+    def Make_Move(self, ID, action_space, action_history):
+        return self.Get_Random_Action()
 
