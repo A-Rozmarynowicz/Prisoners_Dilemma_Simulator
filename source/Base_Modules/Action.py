84e32ce1f673d3b0b47bdd58f5d809de5b50d88d
A-Rozmarynowicz <arkadiuszrozmarynowicz@gmail.com>
2026-05-04 19:02:22 +0200

    reworked Action History class

--- source/Base_Modules/Action.py
+++ source/Base_Modules/Action.py
@@ -14,16 +14,17 @@ Act = TypeVar("Act", bound=Actions)
 class Action_History(Generic[Act]):
     def __init__(self):
         super().__init__()
-        self.action_history : list[Duel_Matrix] = []
+        self.action_history : dict[int, list[Act]] = {}
 
-    def Append_Strategy_Actions(self, strategy_actions : Duel_Matrix) -> None:
-        self.action_history.append(strategy_actions)
+    def Append_Strategy_Actions(self, strategy_actions : dict[int, Act]) -> None:
+        for (id, action) in strategy_actions.items():
+            if self.action_history.get(id) == None:
+                self.action_history[id] = [action]
+            else:
+                self.action_history[id].append(action)
 
-    def Get_Action(self, history_index : int, strategy_ID : int) -> Act:
-        return self.Get_Players_Actions(history_index)[strategy_ID]
-
-    def Get_Strategys_Actions(self, history_index : int) -> Act:
-        return self.action_history[history_index]
+    def Get_All_Strategy_Actions(self, strategy_ID : int) -> list[Act]:
+        return self.action_history[strategy_ID]
 
 class Duel_Matrix(Generic[Act]):
     def __init__(self):
