935bc122d77e4381441555cc6ed5e02f545248c6
A-Rozmarynowicz <arkadiuszrozmarynowicz@gmail.com>
2026-05-04 19:24:05 +0200

    Duel matrix works well

--- source/Base_Modules/Action.py
+++ source/Base_Modules/Action.py
@@ -17,6 +17,10 @@ class Action_History(Generic[Act]):
         self.action_history : dict[int, list[Act]] = {}
 
     def Append_Strategy_Actions(self, strategy_actions : dict[int, Act]) -> None:
+        if len(self.action_history) != 0:
+            for id in strategy_actions.keys():
+                if id not in self.action_history.keys():
+                    raise ValueError("Action History tried to append an invalid key.")
         for (id, action) in strategy_actions.items():
             if self.action_history.get(id) == None:
                 self.action_history[id] = [action]
@@ -26,13 +30,22 @@ class Action_History(Generic[Act]):
     def Get_All_Strategy_Actions(self, strategy_ID : int) -> list[Act]:
         return self.action_history[strategy_ID]
 
+    def Get_Action_History(self) -> dict[int, list[Act]]:
+        return self.action_history
+
 class Duel_Matrix(Generic[Act]):
     def __init__(self):
         super().__init__()
         self.duel_matrix : dict[tuple[int, ...], Action_History[Act]] = {}
 
     def Append_Strategy_Actions(self,  strategy_actions : dict[int, Act]) -> None:
+        if len(self.duel_matrix) != 0:
+            assert(len(next(iter(self.duel_matrix))) ==  len(strategy_actions))
         sorted_indices = tuple(sorted(strategy_actions.keys()))
         if self.duel_matrix.get(sorted_indices) == None:
             self.duel_matrix[sorted_indices] = Action_History[Act]()
         self.duel_matrix[sorted_indices].Append_Strategy_Actions(strategy_actions)
+
+    def Get_Action_History(self, strategy_ids : tuple) -> Action_History:
+        strategy_ids = tuple(sorted(strategy_ids))
+        return self.duel_matrix[strategy_ids]
--- source/main.py
+++ source/main.py
@@ -1,24 +1,54 @@
 from Base_Modules.Environments import Prison
 from Prison_Strategies.Basic_Strategies import *
 from Base_Modules.Game_Master import Game_Master
-from Base_Modules.Action import Action_History
+from Base_Modules.Action import Action_History, Prison_Actions, Duel_Matrix
 
-prison = Prison()
-actions = prison.Get_Actions()
-
-strategies = {
-    0: Random_Strategy(0, actions),
-    1: Always_Betray(1, actions),
-    2: Always_Cooperate(2, actions),
-    3: Random_Strategy(3, actions, p_coop=0.1),
+ss1 = {
+    0 : Prison_Actions.Betray,
+    1 : Prison_Actions.Cooperate,
+    2 : Prison_Actions.Betray,
+}
+ss2 = {
+    0 : Prison_Actions.Cooperate,
+    3 : Prison_Actions.Cooperate,
+    4 : Prison_Actions.Cooperate,
+}
+ss3 = {
+    6 : Prison_Actions.Betray,
+    7 : Prison_Actions.Betray,
+    8 : Prison_Actions.Cooperate,
 }
 
-gm = Game_Master(prison, strategies=strategies)
+# ah = Action_History()
+# ah.Append_Strategy_Actions(ss1)
+# ah.Append_Strategy_Actions(ss2)
+# ah.Append_Strategy_Actions(ss3)
 
-action_history, score = gm.Tournament(10, Game_Master.Game_Type.All_Vs_All, True, 2, False)
-score = asc = {k: v for k, v in sorted(score.items(), key=lambda item: -item[1])}
+dm = Duel_Matrix()
+dm.Append_Strategy_Actions(ss1)
+dm.Append_Strategy_Actions(ss2)
+dm.Append_Strategy_Actions(ss3)
 
-winner = next(iter(score.keys()))
+print(dm.Get_Action_History((6, 7, 8)).Get_Action_History())
 
-print(str(strategies[winner]))
-print(score)
+# print(ah.Get_Action_History())
+
+# prison = Prison()
+# actions = prison.Get_Actions()
+
+# strategies = {
+#     0: Random_Strategy(0, actions),
+#     1: Always_Betray(1, actions),
+#     2: Always_Cooperate(2, actions),
+#     3: Random_Strategy(3, actions, p_coop=0.1),
+# }
+
+# gm = Game_Master(prison, strategies=strategies)
+
+# action_history, score = gm.Tournament(10, Game_Master.Game_Type.All_Vs_All, True, 2, False)
+# score = asc = {k: v for k, v in sorted(score.items(), key=lambda item: -item[1])}
+
+# winner = next(iter(score.keys()))
+
+# print(str(strategies[winner]))
+# print(score)
